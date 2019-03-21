16ba62b502feba4764d1bcdff9c25d7f27e326ac
陈泗政 <chenxiaozheng@chenxiaozhengdeMacBook-Pro.local>
2019-03-21 17:39:14 +0800

    design-patterns

---
+++ src/main/java/设计模式/装饰者/decorator/Milk.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018. 杭州端点网络科技有限公司.  All rights reserved.
+ */
+package 设计模式.装饰者.decorator;
+
+/**
+ * Author: <a href="sizheng.csz@alibaba-inc.com">XiYing</a>
+ * Date: 2019/3/21
+ */
+
+public class Milk {
+
+}
